02eb25757fa21bc8309b656655a40b218f21d158
Freddiefady <dodofady49@gmail.com>
2025-10-23 18:24:34 +0300

    chore: add label and color interface.

--- app/Enums/StatusCoursesEnum.php
+++ app/Enums/StatusCoursesEnum.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
-enum StatusCoursesEnum: string
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+
+enum StatusCoursesEnum: string implements HasColor, HasLabel
 {
     case DRAFT = 'draft';
     case PUBLISHED = 'published';
+
+    public function getLabel(): string
+    {
+        return $this->value;
+    }
+
+    public function getColor(): string
+    {
+        return match ($this) {
+            self::DRAFT => 'secondary',
+            self::PUBLISHED => 'success',
+        };
+    }
 }
